4bd7839439f4233040ddd4938252ad2dbe5648ea
Anthony Hsiao <anthony@sapnasolutions.com>
2010-06-16 11:18:21 +0530

    made it into a primitive webservice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def index
     
   end
+  
+  def ipadify
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   map.connect '/', :controller => 'application', :action => 'index'
+  map.connect '/ipadify.js', :controller => 'application', :action => 'ipadify'
 end
